7ad968f91c976c49bf1fb273858c743c8759bcb0
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-09-10 14:40:38 -0700

    Abdulgaffar

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+
+/**
+* main - Entry point
+* description: prints alphabets
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
