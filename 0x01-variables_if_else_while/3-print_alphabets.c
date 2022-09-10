a9646594a7692de01e012c02807cc1f7b5170c0e
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-09-10 12:37:53 -0700

    Abdulgaffar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,61 +1,30 @@
 #include <stdio.h>
-
+#include <unistd.h>
 
 
 /**
-
  * main - Prints all possible combinations of two different digits,
-
- *        in ascending order, separated by a comma followed by a space.
-
- *
-
+ *description - in ascending order, separated by a comma followed by a space.
  * Return: Always 0.
-
  */
 
 int main(void)
-  
+
 {
-  
-  int digit1, digit2;
-  
 
-  
-  for (digit1 = 0; digit1 < 9; digit1++)
-    
-    {
-      
-      for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-	
-	{
-	  
-	  putchar((digit1 % 10) + '0');
-	  
-	  putchar((digit2 % 10) + '0');
-	  
+int ch;
 
-	  
-	  if (digit1 == 8 && digit2 == 9)
-	    
-	    continue;
-	  
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
 
-	  
-	  putchar(',');
-	  
-	  putchar(' ');
-	  
-	}
-      
-    }
-  
+putchar('\n');
 
-  
-  putchar('\n');
-  
 
-  
-  return (0);
-  
+return (0);
 }
