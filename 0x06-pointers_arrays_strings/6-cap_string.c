92acf324ffbb0bb1a856a0009ffd8b53f04f135f
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-09-21 13:18:42 -0700

    Abdulgaffar

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,35 @@
 /**
- * cap_string - funtion that capital string
- * @s: Address of the string
- * Return: The uppercase @s address
+ * cap_string - capitalizes all words of a string.
+ * @s: string.
+ * Return: string.
  */
 
 char *cap_string(char *s)
 {
-
 int i;
-for (i = 0; s[i] != '\0'; i++)
+i = 0;
+while (s[i] != '\0')
 {
-if ((s[i] == '\ ' || s[i] == '\t' || s[i] == '\n' ||
-
-s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' ||
-s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' ||
-s[i] == '{' || s[i] == '}') && (s[i + 1] >= 97 && s[i + 1] <= 122))
-s[i + 1] -= 32;
+if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+|| s[i - 1] == '\t' || s[i - 1] == ','
+|| s[i - 1] == ';' || s[i - 1] == '!'
+|| s[i - 1] == '?' || s[i - 1] == '"'
+|| s[i - 1] == '(' || s[i - 1] == ')'
+|| s[i - 1] == '{' || s[i - 1] == '}'
+|| s[i - 1] == '.')
+&& (s[i] >= 'a' && s[i] <= 'z'))
+{
+s[i] = s[i] - 32;
+}
+else if ((s[0] >= 97 && s[0] <= 122))
+{
+s[0] = s[0] - 32;
+}
+else
+{
+s[i] = s[i];
+}
+i++;
 }
 return (s);
 }
