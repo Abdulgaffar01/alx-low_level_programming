ac00d31bea657bb0d362dc728b67c5db459e400d
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-09-26 13:02:47 -0700

    Abdulgaffar

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,32 +1,52 @@
+/**
+ * compare - the value in two addreses
+ * @a: address of the first string
+ * @b: address of the second string
+ * Return: integer value
+ */
+
+int compare(const char *a, const char *b)
+{
+
+while (*a && *b)
+
+{
+if (*a != *b)
+
+{
+return (0);
+}
+
+a++;
+b++;
+
+}
+return (*b == '\0');
+}
 
 /**
- * _strstr - function locate
- * @haystack: pointer to char
- * @needle: pointer to char
- * Return: 0
+ * _strstr - function finds the first occurrence of the substring
+ * @needle in the string @haystack
+ * @haystack: address of the string
+ * @needle: address of the substring
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
  */
 
 char *_strstr(char *haystack, char *needle)
+
+{
+while (*haystack != '\0')
 {
 
-char *result = haystack, *fneedle = needle;
-while (*haystack)
+if ((*haystack == *needle) && compare(haystack, needle))
+
+{
+return (haystack);
 }
 
-while (*needle)
-{
-if (*haystack++ != *needle++)
-{
-break;
+haystack++;
 }
-}
-if (!*needle)
-{
-return (result);
-}
-needle = fneedle;
-result++;
-haystack = result;
-}
-return (0);
+
+return ('\0');
 }
