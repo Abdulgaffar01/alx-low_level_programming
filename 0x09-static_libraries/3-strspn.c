0f3552a957faa033e155a396d605d07de415daa2
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-10-02 02:10:22 -0700

    Abdulgafar

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -5,17 +5,17 @@
  * Return: the length
  */
 
-
 int _strspn(char *s, char *accept)
 {
 int i, j,  c = 0;
 for (i = 0; s[i] != '\0'; i++)
-}
+{
 j = 0;
 while (accept[j] != s[i] && accept[j] != '\0')
 ++j;
+
 if (accept[j] == s[i])
-;
+++c;
 else
 break;
 }
